6338d3ee5e3d7a6dc166dc8565da226f86408b74
Mohamed-A-Zaki <mohamedzaki385@gmail.com>
2022-07-19 14:43:30 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+
+  // effect: "coverflow",
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
