17f75910e408561c6dcf279e52cedfca8222e1f5
Mohamed-A-Zaki <mohamedzaki385@gmail.com>
2022-07-29 00:44:20 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -30,3 +30,39 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+/****************** start gallery section *************************/
+
+let gallery_items = document.querySelectorAll(".gallery .item");
+let modal_img = document.querySelector(".modal img");
+let modal_title = document.querySelector(".modal .modal-title");
+
+gallery_items.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    // add image to modal
+    modal_img.src = `img/portfolio-${index + 1}.jpg`;
+    // add image number to modal title
+    modal_title.innerHTML = `Image ${index + 1}`;
+  });
+});
+
+let gallery_tabs = document.querySelectorAll(".gallery .btn-group .btn");
+
+gallery_tabs.forEach((element) => {
+  element.addEventListener("click", () => {
+    // category name
+    let category = element.innerHTML.toLowerCase();
+
+    gallery_items.forEach((ele) => {
+      if (ele.classList.contains(category)) {
+        ele.classList.add("show");
+        ele.classList.remove("hide");
+      } else {
+        ele.classList.add("hide");
+        ele.classList.remove("show");
+      }
+    });
+  });
+});
+
+/****************** end gallery section *************************/
